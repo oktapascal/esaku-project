166e314abbd4d58f1acdfcdddf479ffe6b00a2d6
oktapascal <oktaiscool@gmail.com>
2022-10-05 14:06:44 +0700

    Create kelompok menu controller & controller implementation

---
+++ app/settings/controllers/kelompok_menu_controller_impl.go
@@ -0,0 +1,90 @@
+package controllers
+
+import (
+	"esaku-project/app/settings/models/web"
+	"esaku-project/app/settings/services"
+	"esaku-project/helpers"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type KelompokMenuControllerImpl struct {
+	KelompokMenuService services.KelompokMenuService
+}
+
+func NewKelompokMenuControllerImpl(kelompokMenuService services.KelompokMenuService) *KelompokMenuControllerImpl {
+	return &KelompokMenuControllerImpl{KelompokMenuService: kelompokMenuService}
+}
+
+func (controller *KelompokMenuControllerImpl) Save(writer http.ResponseWriter, request *http.Request) {
+	kelompokMenuRequest := web.KelompokMenuSaveRequest{}
+	helpers.ReadFromRequestBodyJson(request, &kelompokMenuRequest)
+
+	kelompokMenuResponse := controller.KelompokMenuService.Save(request.Context(), kelompokMenuRequest)
+
+	webResponse := helpers.JsonResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   kelompokMenuResponse,
+	}
+
+	helpers.WriteToResponseBodyJson(writer, webResponse)
+}
+
+func (controller *KelompokMenuControllerImpl) Update(writer http.ResponseWriter, request *http.Request) {
+	kelompokMenuRequest := web.KelompokMenuUpdateRequest{}
+	helpers.ReadFromRequestBodyJson(request, &kelompokMenuRequest)
+
+	vars := mux.Vars(request)
+	kodeKlp := vars["kodeKlp"]
+	kelompokMenuRequest.KodeKlp = kodeKlp
+
+	kelompokMenuResponse := controller.KelompokMenuService.Update(request.Context(), kelompokMenuRequest)
+
+	webResponse := helpers.JsonResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   kelompokMenuResponse,
+	}
+
+	helpers.WriteToResponseBodyJson(writer, webResponse)
+}
+
+func (controller *KelompokMenuControllerImpl) Delete(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	kodeKlp := vars["kodeKlp"]
+
+	controller.KelompokMenuService.Delete(request.Context(), kodeKlp)
+
+	webResponse := helpers.JsonResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+	}
+
+	helpers.WriteToResponseBodyJson(writer, webResponse)
+}
+
+func (controller *KelompokMenuControllerImpl) FindById(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	kodeKlp := vars["kodeKlp"]
+
+	klpMenuResponse := controller.KelompokMenuService.FindById(request.Context(), kodeKlp)
+
+	webResponse := helpers.JsonResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   klpMenuResponse,
+	}
+	helpers.WriteToResponseBodyJson(writer, webResponse)
+}
+
+func (controller *KelompokMenuControllerImpl) FindAll(writer http.ResponseWriter, request *http.Request) {
+	klpMenuResponses := controller.KelompokMenuService.FindAll(request.Context())
+
+	webResponse := helpers.JsonResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   klpMenuResponses,
+	}
+	helpers.WriteToResponseBodyJson(writer, webResponse)
+}
