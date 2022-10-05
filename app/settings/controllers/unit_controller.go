6c98d107bd9d51e80a6a5ac102eed2e34e0b6880
oktapascal <oktaiscool@gmail.com>
2022-10-05 14:22:01 +0700

    Create unit controller & controller implementation

---
+++ app/settings/controllers/unit_controller.go
@@ -0,0 +1,11 @@
+package controllers
+
+import "net/http"
+
+type UnitController interface {
+	Save(writer http.ResponseWriter, request *http.Request)
+	Update(writer http.ResponseWriter, request *http.Request)
+	Delete(writer http.ResponseWriter, request *http.Request)
+	FindById(writer http.ResponseWriter, request *http.Request)
+	FindAll(writer http.ResponseWriter, request *http.Request)
+}
---
+++ app/settings/controllers/unit_controller_impl.go
@@ -0,0 +1,88 @@
+package controllers
+
+import (
+	"esaku-project/app/settings/models/web"
+	"esaku-project/app/settings/services"
+	"esaku-project/helpers"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type UnitControllerImpl struct {
+	UnitService services.UnitService
+}
+
+func NewUnitControllerImpl(unitService services.UnitService) *UnitControllerImpl {
+	return &UnitControllerImpl{UnitService: unitService}
+}
+
+func (controller *UnitControllerImpl) Save(writer http.ResponseWriter, request *http.Request) {
+	unitRequest := web.UnitSaveRequest{}
+	helpers.ReadFromRequestBodyJson(request, unitRequest)
+
+	unitResponse := controller.UnitService.Save(request.Context(), unitRequest)
+
+	webResponse := helpers.JsonResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   unitResponse,
+	}
+
+	helpers.WriteToResponseBodyJson(writer, webResponse)
+}
+
+func (controller *UnitControllerImpl) Update(writer http.ResponseWriter, request *http.Request) {
+	unitRequest := web.UnitUpdateRequest{}
+	helpers.ReadFromRequestBodyJson(request, unitRequest)
+
+	unitResponse := controller.UnitService.Update(request.Context(), unitRequest)
+
+	webResponse := helpers.JsonResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   unitResponse,
+	}
+
+	helpers.WriteToResponseBodyJson(writer, webResponse)
+}
+
+func (controller *UnitControllerImpl) Delete(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	kodeUnit := vars["kodeUnit"]
+
+	controller.UnitService.Delete(request.Context(), kodeUnit)
+
+	webResponse := helpers.JsonResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+	}
+
+	helpers.WriteToResponseBodyJson(writer, webResponse)
+}
+
+func (controller *UnitControllerImpl) FindById(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	kodeUnit := vars["kodeUnit"]
+
+	unitResponse := controller.UnitService.FindById(request.Context(), kodeUnit)
+
+	webResponse := helpers.JsonResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   unitResponse,
+	}
+
+	helpers.WriteToResponseBodyJson(writer, webResponse)
+}
+
+func (controller *UnitControllerImpl) FindAll(writer http.ResponseWriter, request *http.Request) {
+	unitResponse := controller.UnitService.FindAll(request.Context())
+
+	webResponse := helpers.JsonResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   unitResponse,
+	}
+
+	helpers.WriteToResponseBodyJson(writer, webResponse)
+}
