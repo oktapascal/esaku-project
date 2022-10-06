dfdb4ca592620054cf93ed91af59c305687a1ecb
oktapascal <oktaiscool@gmail.com>
2022-10-06 15:50:27 +0700

    Create karyawan domain

---
+++ app/settings/models/domain/karyawan.go
@@ -0,0 +1,16 @@
+package domain
+
+type Karyawan struct {
+	Nik        string
+	KodeLokasi string
+	Nama       string
+	KodeUnit   string
+	FlagAktif  bool
+	Jabatan    string
+	Alamat     string
+	NoTelp     string
+	NoHp       string
+	Email      string
+	Foto       []byte
+	Unit
+}
